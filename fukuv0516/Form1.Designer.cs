e1aa0dfe1807145a7fa06869393a26049c3c4c3c
YuTanaka <tanakaedu@users.noreply.github.com>
2025-05-16 14:04:51 +0900

    復習問題

--- fukuv0516/Form1.Designer.cs
+++ fukuv0516/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F);
+            label1.Location = new Point(296, 208);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 45);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(586, 250);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 46);
+            button1.TabIndex = 1;
+            button1.Text = "↑";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(550, 302);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 45);
+            button2.TabIndex = 2;
+            button2.Text = "←";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(631, 302);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 45);
+            button3.TabIndex = 3;
+            button3.Text = "→";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(586, 353);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 46);
+            button4.TabIndex = 4;
+            button4.Text = "↓";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(718, 421);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- fukuv0516/Form1.cs
+++ fukuv0516/Form1.cs
@@ -6,5 +6,31 @@ namespace fukuv0516
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 20;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 20;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 20;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 20;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "tanaka";
+            label1.ForeColor = Color.White;
+        }
     }
 }
